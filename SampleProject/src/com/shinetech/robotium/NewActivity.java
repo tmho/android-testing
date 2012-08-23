c37cffde75b6b98d6d37cc7a4dd2fe459629ce1c
Tim <tim@fry.(none)>
2012-08-23 11:43:04 +1000

    added example04

--- SampleProject/src/com/shinetech/robotium/NewActivity.java
+++ SampleProject/src/com/shinetech/robotium/NewActivity.java
@@ -2,14 +2,29 @@ package com.shinetech.robotium;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class NewActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_activity);
+		
+		final TextView example04TextView = (TextView) findViewById(R.id.example_04_text_view);
+		final EditText example04EditText = (EditText) findViewById(R.id.example_04_edit_text);
+		
+		final Button example04Button = (Button) findViewById(R.id.example_04_button);
+		example04Button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				example04TextView.setText(example04EditText.getText().toString());
+			}
+		});
 	}
 }
